8aec9fd8533e9cd007a4f085696e3df1f2994168
kly001 <karenly1961@gmail.com>
2020-06-17 20:21:48 -0400

    NavigationBar setup

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -1,30 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import Home from "./components/Home";
-import Game from "./components/Game"
+import NavigationBar from './components/NavigationBar';
 import './App.css';
 
-const App = () => {
-  return (
-    <Router>
-      <nav className="navbar">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/game">Game</Link>
-        </li>
-      </nav>
+
+class App extends React.Component {
+  render() {
+    return (
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/game" component={Game} />
-        </Switch>
+        <NavigationBar />
       </div>
-
-
-    </Router>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ gameoflife/src/components/NavigationBar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
+import Home from './Home';
+import Game from './Game';
+
+
+class NavigationBar extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+          <div>
+            <div>
+              <nav className="navbar navbar-expand-md bg-dark navbar-dark">
+                <div className="container-fluid">
+                  <NavLink className="navbar-brand" exact to="/" >React Navbar</NavLink>
+                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul className="nav navbar-nav">
+                      <li className="nav-item">
+                        <NavLink className="nav-link" exact to="/" >Home</NavLink>
+                      </li>
+                      <li>
+                        <NavLink className="nav-link" exact to="/game">Game</NavLink>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+              <div className="content">
+              <Switch>
+                <Route path="/" component={Home} exact />  
+                <Route path="/game" component={Game} exact />
+              </Switch>
+            </div>
+            </div>
+          </div>
+        </BrowserRouter>
+    );
+  }
+}
+export default NavigationBar;
